5ad8ea3d7f846eec838745c66f94c37738f8f814
SinchanaMC <121946647+SinchanaMC@users.noreply.github.com>
2024-09-02 18:57:54 +0530

    Update project.js

--- project.js
+++ project.js
@@ -2,7 +2,8 @@ document.addEventListener('DOMContentLoaded', function () {
     const menuIcon = document.getElementById('menu-icon');
     const navbar = document.getElementById('navbar');
 
-    menuIcon.addEventListener('click', function () {
+    menuIcon.addEventListener('click', function() {
+        console.log('Menu icon clicked'); // Debugging: check if click is detected
         navbar.classList.toggle('show');
     });
 });
